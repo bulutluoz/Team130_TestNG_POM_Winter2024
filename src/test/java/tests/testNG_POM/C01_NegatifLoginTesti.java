53e1aea6e1423dc7d0e59fc0e2cccd46942c0bba
bulutluoz <abbulutluoz@gmail.com>
2024-01-29 19:18:25 +0100

    17.gun sonu

---
+++ src/test/java/tests/testNG_POM/C01_NegatifLoginTesti.java
@@ -0,0 +1,77 @@
+package tests.testNG_POM;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.TestOtomasyonPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class C01_NegatifLoginTesti {
+
+    //1- https://www.testotomasyonu.com/ anasayfasina gidin
+    //2- account linkine basin
+    //3-  3 farkli test method’u olusturun.
+    //	- gecerli email, gecersiz password
+    //	- gecersiz email, gecerli password
+    //	- gecersiz email, gecersiz password.
+    //4- SignIn butonuna basarak login olun
+    //5- Basarili olarak giris yapilamadigini test edin
+
+
+    @Test
+    public void gecersizPasswordTesti(){
+        //1- https://www.testotomasyonu.com/ anasayfasina gidin
+        Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
+        //2- account linkine basin
+        TestOtomasyonPage testOtomasyonPage = new TestOtomasyonPage();
+        testOtomasyonPage.accountLinki.click();
+        //3-  3 farkli test method’u olusturun.
+        //	- gecerli email, gecersiz password
+        testOtomasyonPage.emailKutusu.sendKeys(ConfigReader.getProperty("toGecerliEmail"));
+        testOtomasyonPage.passwordKutusu.sendKeys(ConfigReader.getProperty("toGecersizPassword"));
+        //4- SignIn butonuna basarak login olun
+        testOtomasyonPage.loginButonu.click();
+        //5- Basarili olarak giris yapilamadigini test edin
+        Assert.assertTrue(testOtomasyonPage.emailKutusu.isDisplayed());
+
+        Driver.quitDriver();
+    }
+
+    @Test
+    public void gecersizEmailTesti(){
+        //1- https://www.testotomasyonu.com/ anasayfasina gidin
+        Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
+        //2- account linkine basin
+        TestOtomasyonPage testOtomasyonPage = new TestOtomasyonPage();
+        testOtomasyonPage.accountLinki.click();
+        //3-  3 farkli test method’u olusturun.
+        //	- gecersiz email, gecerli password
+        testOtomasyonPage.emailKutusu.sendKeys(ConfigReader.getProperty("toGecersizEmail"));
+        testOtomasyonPage.passwordKutusu.sendKeys(ConfigReader.getProperty("toGecerliPassword"));
+        //4- SignIn butonuna basarak login olun
+        testOtomasyonPage.loginButonu.click();
+        //5- Basarili olarak giris yapilamadigini test edin
+        Assert.assertTrue(testOtomasyonPage.emailKutusu.isDisplayed());
+
+        Driver.quitDriver();
+    }
+
+    @Test
+    public void gecersizPasswordGecersizEmailTesti(){
+        //1- https://www.testotomasyonu.com/ anasayfasina gidin
+        Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
+        //2- account linkine basin
+        TestOtomasyonPage testOtomasyonPage = new TestOtomasyonPage();
+        testOtomasyonPage.accountLinki.click();
+        //3-  3 farkli test method’u olusturun.
+        //	- gecersiz email, gecersiz password.
+        testOtomasyonPage.emailKutusu.sendKeys(ConfigReader.getProperty("toGecersizEmail"));
+        testOtomasyonPage.passwordKutusu.sendKeys(ConfigReader.getProperty("toGecersizPassword"));
+        //4- SignIn butonuna basarak login olun
+        testOtomasyonPage.loginButonu.click();
+        //5- Basarili olarak giris yapilamadigini test edin
+        Assert.assertTrue(testOtomasyonPage.emailKutusu.isDisplayed());
+
+        Driver.quitDriver();
+    }
+}
---
+++ src/test/java/tests/testNG_POM/C02_UrunSepetiTesti.java
@@ -0,0 +1,17 @@
+package tests.testNG_POM;
+
+import org.testng.annotations.Test;
+
+public class C02_UrunSepetiTesti {
+
+    @Test
+    public void urunSepetiTesti(){
+        //1- https://www.testotomasyonu.com/ anasayfasina gidin
+        //2- phone icin arama yapin
+        //3- Listelenen sonuclardan ilkini tiklayin
+        //4- urun ismini kaydedin ve sepete ekleyin
+        //5- your cart linkine tiklayin
+        //6- kaydettiginiz urun ismi ile sepetteki urun isminin ayni oldugunu test edin
+        //7- sayfayi kapatin
+    }
+}
