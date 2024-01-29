8ad823e6324705e630c505408d0e7cf524e316fd
bulutluoz <abbulutluoz@gmail.com>
2024-01-29 20:44:42 +0100

    17.gun sonu

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,6 +3,9 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
@@ -16,9 +19,41 @@ public class Driver {
 
     public static WebDriver getDriver(){
 
-        WebDriverManager.chromedriver().setup();
+        String browserTercihi = ConfigReader.getProperty("browser");
+        /*
+            Browser'in sadece chrome olmamasi icin
+            configuration.properties'e browser = firefox
+            secenegi ekledik.
+
+            Orada yazan browser tercihini 22.satirda alip
+            tercihe uygun driver olusturmasi icin
+            bir switch statement kullandik
+         */
+
+
         if (driver == null){
-            driver = new ChromeDriver();
+
+            switch (browserTercihi){
+
+                case "firefox" :
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+
+                case "edge" :
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+
+                case "safari" :
+                    WebDriverManager.safaridriver().setup();
+                    driver = new SafariDriver();
+                    break;
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+            }
+
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         }
