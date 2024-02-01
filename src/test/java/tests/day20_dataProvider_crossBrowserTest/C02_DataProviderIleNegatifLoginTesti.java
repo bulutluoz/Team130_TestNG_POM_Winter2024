c29930eada6c86a1de0ba969e97255008cf65f16
bulutluoz <abbulutluoz@gmail.com>
2024-02-01 19:22:58 +0100

    20.gun sonu

---
+++ src/test/java/tests/day20_dataProvider_crossBrowserTest/C02_DataProviderIleNegatifLoginTesti.java
@@ -0,0 +1,60 @@
+package tests.day20_dataProvider_crossBrowserTest;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pages.TestOtomasyonPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class C02_DataProviderIleNegatifLoginTesti {
+
+
+    /* test otomasyonu anasayfaya gidin
+       account linkine tiklayin
+       liste olarak verilen kullanici adi ve password'ler ile
+       giris yapilamadigini test edin
+
+        banu@gmail.com     898989
+        sedat@yahoo.com    989887
+        orkong@tmail.com   122334
+        fatih@hotmail.com  454545
+        arzu@senmail.com   676767
+        mehmet@mynet.com   878987
+     */
+
+    @DataProvider
+    public static Object[][] emailVePasswordProvider() {
+
+        String[][] emailVeSifreler = {  {"banu@gmail.com","898989"},
+                                        {"sedat@yahoo.com","989887"},
+                                        {"orkong@tmail.com","122334"},
+                                        {"fatih@hotmail.com","454545"},
+                                        {"arzu@senmail.com","676767"},
+                                        {"mehmet@mynet.com","878987"}};
+
+        return new Object[][]{};
+    }
+
+
+    @Test(dataProvider = "emailVePasswordProvider")
+    public void topluNegatifLoginTesti(String email , String password){
+
+        Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
+
+        TestOtomasyonPage testOtomasyonPage = new TestOtomasyonPage();
+
+        testOtomasyonPage.accountLinki.click();
+
+        testOtomasyonPage.emailKutusu.sendKeys(email);
+        testOtomasyonPage.passwordKutusu.sendKeys(password);
+
+        testOtomasyonPage.loginButonu.click();
+
+        Assert.assertTrue(testOtomasyonPage.emailKutusu.isDisplayed());
+
+        Driver.quitDriver();
+
+    }
+
+}
